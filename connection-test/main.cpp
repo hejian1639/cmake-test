535df7981c601850cb825fbae5e2cc4205acb497
liebao <liebao@localhost.localdomain>
2019-02-15 10:33:47 +0800

    apply scan code

--- connection-test/main.cpp
+++ connection-test/main.cpp
@@ -1,112 +1,94 @@
-//
-// Created by darren on 18-8-22.
-//
-
-#include <netdb.h>
-#include <errno.h>
-#include <string.h>
-#include <arpa/inet.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <iostream>
-
-
-int getLocalPort() {
-    unsigned short port = 0;
-    int sock = socket(AF_INET, SOCK_STREAM, 0);
-    if (sock < 0) return port;
-    struct sockaddr_in addr;
-    addr.sin_family = AF_INET;
-    addr.sin_addr.s_addr = htonl(INADDR_ANY);
-    addr.sin_port = 0;        // 若port指定为0,则调用bind时，系统会为其指定一个可用的端口号
-    int ret = bind(sock, (struct sockaddr *) &addr, sizeof addr);    // 3. 绑定
-    do {
-        if (0 != ret) {// 4. 利用getsockname获取
-            //LOGE("upn bind, %d, %s", errno, strerror(errno))
-            break;
-        }
-        struct sockaddr_in sockaddr;
-        int len = sizeof(sockaddr);
-        ret = getsockname(sock, (struct sockaddr *) &sockaddr, (socklen_t *) &len); // this function now not be hooked
+#include<stdlib.h>
+#include<stdio.h>
+#include<sys/socket.h>
+#include<netdb.h>
+#include<string.h>
+#include<unistd.h>
+#include<netinet/in.h>
+#include<arpa/inet.h>
+#include<fcntl.h>
+#include<time.h>
+#include<sys/types.h>
+#define TIMEOUT 5 //由于把socket设置为非阻塞，使用select函数，观察其
+                //5秒后的是否连接成功，连接不成功则认为其端口没有开放
+ 
+ 
+ 
+struct servenet{
+    char * s_name;
+	char** s_aliases;
+	int    s_port;
+	char*  s_proto;
+};
+ 
+ 
+//参数是目标机的IP
+int main(int argc,char** argv)
+{
+	struct sockaddr_in server;
+	int ret;
+	int len;
+	int scanport;
+    int start_port=0;
+    int end_port=1024;
+	int sockfd;
+ 
+	fd_set rset;
+	fd_set wset; 
+        struct servent *sp;
+ 
+	
+ 
+       
+    for(scanport=start_port;scanport<end_port;scanport++)
+    {
+ 
+ 
+    	if (-1==(sockfd=socket(AF_INET,SOCK_STREAM,0)))
+		{
+		perror("can not create socket\n");
+		exit(1);
+		}
+		
+		memset(&server,0,sizeof(struct sockaddr_in));
+		server.sin_family = AF_INET;
+		server.sin_addr.s_addr = inet_addr(argv[1]);
+		server.sin_port = htons(scanport);
+ 		
+                int flag = fcntl(sockfd, F_GETFL,0);
+		fcntl(sockfd,F_SETFL, flag|O_NONBLOCK);
+		struct timeval tm;
+		tm.tv_sec = TIMEOUT;
+		tm.tv_usec = 0;
         
-        if (0 != ret) {
-            // LOGE("upn, getsockname:%d, %s", errno, strerror(errno))
-            break;
-        }
-        port = ntohs(sockaddr.sin_port); // 获取端口号
-    } while (false);
-    //LOGD("UpnConfig::port::%d", port)
-    close(sock);
-    
-    return port;
+		//connect为非阻塞，连接不成功立即返回-1
+		if (!connect(sockfd,(struct sockaddr*)&server,sizeof(struct sockaddr))){
+			 sp=getservbyport(htons(scanport),"tcp");
+ 
+        	printf("tcp port %d open:%s\n ",scanport,sp->s_name);
+ 
+			
+                }//假如连接不成功，则运行select,直到超时
+		else {
+			FD_ZERO(&rset);
+			FD_ZERO(&wset);
+			FD_SET(sockfd, &rset);
+			FD_SET(sockfd, &wset);
+			int error; //错误代码
+			socklen_t len = sizeof(error); 
+			//5秒后查看socket的状态变化 
+			if (select(sockfd+1,&rset,&wset,NULL,&tm)>0){
+				getsockopt(sockfd, SOL_SOCKET, SO_ERROR,&error, &len );
+				if(error == 0)
+					printf("Port %d is opened\n", scanport);
+			}
+		
+		}
+		close(sockfd);
+ 
+	}
+		return 0;
+ 
 }
 
-int getConnectState(int port, const char *ip, int tv_sec, int tv_usec) {
-    
-    int fd = -1;
-    int ret = 0;
-    do {
-        fd = socket(AF_INET, SOCK_STREAM, 0);
-        if (fd < 0) {
-            printf("Test::connect:socket: %d, %s\n", errno, strerror(errno));
-            break;
-        }
-        struct sockaddr_in addr;
-        bzero(&addr, sizeof(addr));
-        addr.sin_family = AF_INET;
-        addr.sin_port = htons(port);
-        addr.sin_addr.s_addr = inet_addr(ip);
-        
-        int flags = fcntl(fd, F_GETFL, 0);
-        if (flags < 0) {
-            printf("Test::connect:get flags: %d, %s\n", errno, strerror(errno));
-            break;
-        }
-        flags |= O_NONBLOCK;
-        if (fcntl(fd, F_SETFL, (void *) flags) < 0) {
-            printf("Test::connect:set flags: %d, %s\n", errno, strerror(errno));
-            break;
-        }
-        if (0 == connect(fd, (struct sockaddr *) &addr, sizeof(addr))) {
-            break;
-        }
-        printf("Test::connect:failed: %d, %s\n", errno, strerror(errno));
-#ifdef __ENABLE_LOG_D__
-        if (EINPROGRESS != errno) {
-            LOGE("Test::connect:failed: %d, %s", errno, strerror(errno));
-            break;
-        }
-#endif
-        fd_set fdr, fdw;
-        FD_ZERO(&fdr);
-        FD_ZERO(&fdw);
-        FD_SET(fd, &fdr);
-        FD_SET(fd, &fdw);
-        
-        struct timeval timeout;
-        timeout.tv_sec = tv_sec;// seconds
-        timeout.tv_usec = tv_usec;
-        int rc = select(fd + 1, &fdr, &fdw, NULL, &timeout);
-        if (0 < rc && (FD_ISSET(fd, &fdw) || FD_ISSET(fd, &fdr))) {
-            break;
-        }
-        if (rc < 0) {
-            printf("Test::connect:select: %d, %d, %s\n", rc, errno, strerror(errno));
-            break;
-        }
-        if (0 == rc) {
-            printf("Test::connect:time out: %d, %s\n", errno, strerror(errno));
-            break;
-        }
-    } while (false);
-    if (0 <= fd) {
-        close(fd);
-    }
-    return ret;
-}
 
-int main(int argc, const char * argv[]) {
-    getConnectState(80, "localhost", 1, 0);
-    
-    return 0;
-}
